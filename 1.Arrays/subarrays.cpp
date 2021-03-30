e08980fe741f50c739b1174181b478cfb5f3498a
dipscoder <dipeshjaiswal140@gmail.com>
2021-03-30 16:42:42 +0530

    update

--- 1.Arrays/subarrays.cpp
+++ 1.Arrays/subarrays.cpp
@@ -15,7 +15,7 @@ int main (){
     // Subarrays
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < n; j++)
+        for (int j = i; j < n; j++)
         {
             for (int k = i; k <= j; k++)
             {
